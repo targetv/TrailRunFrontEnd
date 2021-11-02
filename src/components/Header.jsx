0db17160fba73ea73f52e633f4d4b05838793664
Sean Davison <seandavison1998@hotmail.com>
2021-11-02 03:53:37 +0000

    Fixed deployment issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,120 +1,117 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import styled from "styled-components";
-import {FaHome} from "react-icons/fa";
-import {HiOutlineMail} from "react-icons/hi"
-import Logo from "../images/logo_activelife.png"
+import { FaHome } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import Logo from "../images/logo_activelife.png";
 import { Button } from "./Button";
 import { useLocation, useHistory } from "react-router";
 
-
-
 const HeaderContainer = styled.header`
+  display: grid;
+  grid-template-columns: 2fr 7fr 1fr 2fr;
+  place-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: white;
 
-display: grid;
-grid-template-columns: 2fr 7fr 1fr 2fr;
-place-items: center;
-position: sticky;
-top: 0;
-z-index: 10;
-background-color: white;
-
-
-img{
+  img {
     width: 144px;
-    height: 70px; 
-}
+    height: 70px;
+  }
 
-li{
+  li {
     display: inline-block;
     margin-left: 20px;
+  }
 
-}
-
-ul{
+  ul {
     list-style-type: none;
     padding: 0;
+  }
 
-}
-
-.icon{
+  .icon {
     font-size: 35px;
-    
-}
+  }
 
-.fillBlue{
+  .fillBlue {
     fill: var(--blue);
-    :hover{
-        fill: var(--yellow);
+    :hover {
+      fill: var(--yellow);
     }
+  }
+  button {
+    border: none;
+    background: none;
+  }
 
-}
-
-.outlineBlue{
+  .outlineBlue {
     color: var(--blue);
-    :hover{
-        color: var(--yellow);
+    :hover {
+      color: var(--yellow);
     }
-}
-
-`
+  }
+`;
 
 const NavButtons = styled.a`
-    color: var(--blue);
-    text-decoration: none;
-    :hover{
-       color: var(--yellow);
-    }
+  color: var(--blue);
+  text-decoration: none;
+  :hover {
+    color: var(--yellow);
+  }
+`;
 
-`
+function Header({ setModal, modalOn, userLoggedIn }) {
+  const location = useLocation();
+  const history = useHistory();
 
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="logo" />
+      {location.pathname === "/home" ? (
+        <nav>
+          <ul>
+            <li>
+              <NavButtons href="#aboutUs">About Us</NavButtons>
+            </li>
+            <li>
+              <NavButtons href="#sponsors">Sponsors</NavButtons>
+            </li>
+            <li>
+              <NavButtons href="#location">Location</NavButtons>
+            </li>
+            <li>
+              <NavButtons href="#registraion">Registration</NavButtons>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <h3>Welcome</h3>
+      )}
 
-function Header({setModal, modalOn, userLoggedIn}){
+      <Button
+        onClick={() => {
+          setModal(!modalOn);
+        }}
+      >
+        {userLoggedIn ? "Sign Out " : "Sign In"}
+      </Button>
 
- 
-    const location = useLocation();
-    const history = useHistory();
-
-
-  
-
-    return(
-        <HeaderContainer>
-        
-           <img src={Logo} alt="logo"/>
-            {location.pathname === "/home" ?  <nav>
-               <ul>
-                   <li>
-                       <NavButtons href="#aboutUs">About Us</NavButtons>
-                   </li>
-                   <li>
-                       <NavButtons href="#sponsors">Sponsors</NavButtons>
-                   </li>
-                   <li>
-                       <NavButtons href="#location">Location</NavButtons>
-                   </li>
-                   <li>
-                       <NavButtons href="#registraion">Registration</NavButtons>
-                   </li>
-               </ul>
-            </nav> : <h3>Welcome</h3> }
-            
-            <Button onClick={() => {
-                setModal(!modalOn)
-            }}>{userLoggedIn ? "Sign Out " : "Sign In"}</Button>
-
-            <ul>
-               
-                <li>
-                    <a onClick={() => {history.push("/")}}><FaHome className="icon fillBlue"/></a>
-                </li>
-                <li>
-                    <HiOutlineMail className="icon outlineBlue"/>
-                </li>
-            </ul>
-         
-
-   
-          
-        </HeaderContainer>
-    )
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              history.push("/home");
+            }}
+          >
+            <FaHome className="icon fillBlue" />
+          </button>
+        </li>
+        <li>
+          <HiOutlineMail className="icon outlineBlue" />
+        </li>
+      </ul>
+    </HeaderContainer>
+  );
 }
 export default Header;

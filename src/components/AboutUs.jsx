114bf7479bcf9b495d8ab2c64c691478b5b6776b
Sean Davison <seandavison1998@hotmail.com>
2022-05-13 10:21:38 +0200

    Added theme and updated about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,51 +1,51 @@
 import styled from "styled-components";
-import Logo from "../images/logo_activelife.png"
-
+import Logo from "../images/logo_activelife.png";
+import { rem } from "polished";
 
 const AboutUsContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-height: 600px;
-place-content: center;
-grid-gap: 50px;
-color: var(--blue);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(${rem("200px")}, 1fr));
+  height: ${rem("600px")};
+  place-items: center;
+  grid-gap: ${rem("50px")};
+  color: ${(props) => props.theme.blue};
 
-
-
-h2{
+  h2 {
     font-size: 2.5rem;
-}
+  }
 
-h3{
+  h3 {
     font-size: 1.8rem;
-}
+  }
 
-p{
+  p {
     text-align: center;
-}
+  }
 
-.aboutUsInformation{
+  .aboutUsInformation {
     display: grid;
     place-items: center;
-    grid-gap: 10px;
+    grid-gap: ${rem("10px")};
+  }
+`;
+
+function AboutUs() {
+  return (
+    <AboutUsContainer className="container80" id="aboutUs">
+      <div className="aboutUsInformation">
+        <h2>About Us</h2>
+        <h3>ARC 21/279</h3>
+        <p>
+          Coxhoe Trail Run 7th Successful Year Running A scenic route full of
+          interest and undulating over old railway lines and quarry paths
+          between Coxhoe and Kelloe in County Durham. Start at Public byway
+          (Coxhoe Hall), enter the path leading to Old Kelloe Way and follow the
+          signposted Limestone LinX route (loop-shaped), ending back near the
+          start point. Route
+        </p>
+      </div>
+      <img src={Logo} alt="" />
+    </AboutUsContainer>
+  );
 }
-
-
-
-
-`
-
-function AboutUs(){
-    return(
-        <AboutUsContainer className="container80" id="aboutUs">
-        <div className="aboutUsInformation">
-            <h2>About Us</h2>
-            <h3>ARC 21/279</h3>
-            <p>Coxhoe Trail Run 7th Successful Year Running A scenic route full of interest and undulating over old railway lines and quarry paths between Coxhoe and Kelloe in County Durham. Start at Public byway (Coxhoe Hall), enter the path leading to Old Kelloe Way and follow the signposted Limestone LinX route (loop-shaped), ending back near the start point. Route</p>
-        </div>
-        <img src={Logo} alt="" />
-        
-        </AboutUsContainer>
-    )
-}
-export default AboutUs
+export default AboutUs;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/Theme.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StyledEngineProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <StyledEngineProvider injectFirst>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StyledEngineProvider>
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById("root")
 );

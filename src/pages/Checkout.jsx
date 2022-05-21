97d22d86c4fe7dab9eba20c8a86b1199f02be8cb
Sean Davison <seandavison1998@hotmail.com>
2022-05-21 16:08:58 +0100

    Update Cors

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -49,6 +49,7 @@ export default function App({ item, userId }) {
       }
       fetch(`${process.env.REACT_APP_EXPRESS}/create-payment-intent`, {
         method: "POST",
+        credentials: "same-origin",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ item, email }),
       })
